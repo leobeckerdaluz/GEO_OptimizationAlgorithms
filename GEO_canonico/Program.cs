4d42a32bc354b3a65f48ccd7e47a14ea5b6a3269
leobeckerdaluz <leobeckerdaluz@gmail.com>
2020-11-03 20:32:06 -0300

    Inicializacao GEO canonico

---
+++ GEO_canonico/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GEO_canonico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
