40ed45b4e3b538aa6a948154542640f96fae2605
leobeckerdaluz <leobeckerdaluz@gmail.com>
2020-12-15 22:11:17 -0300

    funcao GEOvar ordenacao e flip de bits + organizacao repositorio

--- GEO.cs
+++ GEO.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Runtime.InteropServices;
 
 
-namespace GEO_canonico
+namespace GEO
 {
     /*
         Classe para armazenar as informações da mutação de um bit.
@@ -24,7 +24,7 @@ namespace GEO_canonico
     /*
         Classe principal contendo os algoritmos GEO
     */
-    public class GEO_canonico
+    public class GEO
     {
         // Inicializa a variável global para a genração de números aleatórios
         public static Random random = new Random();
@@ -192,117 +192,107 @@ namespace GEO_canonico
 
 
 
-
-//         public static List<bool> GEOvar_ordena_e_flipa_um_bit(List<bool> populacao_de_bits, List<double> deltaV_fitness, List<int> bits, int n_variaveis_projeto, int bits_por_variavel, int tamanho_populacao_bits, double tao){
-//============================================================
-//             // Ordena os bits por variável conforme os indices fitness
-//============================================================
-
-//             List<bool> nova_populacao = new List<bool>();
-
-// #if DEBUG_CONSOLE
-//             Console.WriteLine("Antes da ordenação");
-//             for (int i=0; i<deltaV_fitness.Count; i++){
-//                 Console.WriteLine(i + ": Bit: " + bits[i] + " | deltaV_fitness: " + deltaV_fitness[i]);
-//             }
-// #endif
-
-//             // Para cada variável de projeto
-//             // Percorre bit a bit para cada variável
+        /*
+            A função para ordenar e flipar um bit por variável tem como objetivo ordenar todos os bits
+            de cada variável e, posteriormente, escolher com probabilidade normalmente distribuida, um 
+            bit por variável para mutar
+            A função retorna a nova população de bits com os bits mutados.
+        */
+        public static List<bool> GEOvar_ordena_e_flipa_bits(List<bool> populacao_de_bits, List<BitVerificado> lista_informacoes_mutacao, int n_variaveis_projeto, int bits_por_variavel_projeto, int tamanho_populacao_bits, double tao){
             
-//             List<bool> lista_temporaria = new List<bool>();
+            //============================================================
+            // Ordena os bits conforme os indices fitness
+            //============================================================
 
-//             for(int i=0; i<tamanho_populacao_bits; i++){
-//                 lista_temporaria.Add( populacao_de_bits[i] );
-
-                    // Transforma o genótipo de cada variável em uma string para depois converter para decimal
-                    // for (int i=0; i<n_variaveis_projeto; i++){
-                    //     // Cria string representando os bits da variável
-                    //     string fenotipo_xi = "";
-                        
-                    //     // Percorre o número de bits de cada variável de projeto
-                    //     for(int c = bits_por_variavel_projeto*i; c < bits_por_variavel_projeto*(i+1); c++){
-                    //         // Se o bit for true, concatena "1", senão, "0"
-                    //         fenotipo_xi += (cromossomo[c] ? "1" : "0");
-                    //     }
+            List<BitVerificado> depois_ordenar = new List<BitVerificado>();
 
 
-//                 // Se o iterador estiver 1 antes do tamanho da variável, obtém a lista
-//                 if (i % (bits_por_variavel-1) == 0){
-                    
-//                     // Ordena a lista
+#if DEBUG_CONSOLE
+            Console.WriteLine("Antes da ordenação:");
+            Console.WriteLine(lista_informacoes_mutacao.Count);
+            ApresentaCromossomoBool(populacao_de_bits);
+            for (int i=0; i<lista_informacoes_mutacao.Count; i++){
+                Console.WriteLine(i + ": Bit: " + lista_informacoes_mutacao[i].indice_bit_mutado + " | deltaV_fitness: " + lista_informacoes_mutacao[i].delta_fitness);
+            }
+#endif
+
+            // Percorre cada variável de projeto para ordenar os bits e escolher o bit para filpar
+            for (int i=0; i<n_variaveis_projeto; i++){
+                // Cria uma lista com as informações de mutação de cada bit da variável
+                List<BitVerificado> lista_informacoes_bits_variavel = new List<BitVerificado>();
                 
-//                     // Transforma a população e os fitness para array para poder ordenar
-//                     var deltaV_fitness_array = deltaV_fitness.ToArray();
-//                     var bits_array = bits.ToArray();
+                // Percorre o número de bits de cada variável de projeto
+                for(int c = bits_por_variavel_projeto*i; c < bits_por_variavel_projeto*(i+1); c++){
+                    // Se o bit for true, concatena "1", senão, "0"
+                    lista_informacoes_bits_variavel.Add( lista_informacoes_mutacao[c] );
+                }
+
+                // Ordena esses bits da variável
+                lista_informacoes_bits_variavel.Sort(delegate(BitVerificado b1, BitVerificado b2) { return b1.delta_fitness.CompareTo(b2.delta_fitness); });
+
+                // Coloca esses bits ordenado na lista geral
+                foreach (BitVerificado bit in lista_informacoes_bits_variavel){
+                    depois_ordenar.Add(bit);
+                }
+
+
+                //============================================================
+                // Flipa um bit da variável com probabilidade Pk
+                //============================================================
+
+                // Verifica as probabilidades até que um bit seja ummutado
+                bool bit_flipado = false;
+                while (!bit_flipado){
+
+                    // Gera um número aleatório com distribuição uniforme
+                    double ALE = random.NextDouble();
+
+                    // k é o índice da população de bits ordenada
+                    int k = random.Next(1, bits_por_variavel_projeto+1);
                     
-//                     // Ordena a população com base no fitness
-//                     Array.Sort(deltaV_fitness_array, bits_array);
-                    
-//                     // Converte novamente os arrays para listas
-//                     deltaV_fitness = deltaV_fitness_array.ToList();
-//                     bits = bits_array.ToList();
+                    // Probabilidade Pk => k^(-tao)
+                    double Pk = Math.Pow(k, -tao);
+
+                    // k precisa ser de 1 a N, mas aqui nos índices começa em 0
+                    k -= 1;
+
+    #if DEBUG_CONSOLE
+                    Console.WriteLine("Tentando flipar o indice "+k+", que é o bit "+lista_informacoes_bits_variavel[k].indice_bit_mutado+" com Pk "+Pk+" >= ALE "+ALE);
+    #endif
+
+                    // Se o Pk é maior ou igual ao aleatório, então flipa o bit
+                    if (Pk >= ALE){
+
+    #if DEBUG_CONSOLE
+                        Console.WriteLine("Antes de flipar:");
+                        ApresentaCromossomoBool(populacao_de_bits);
+                        Console.WriteLine("Flipando o indice " + k + ", que é o bit " + lista_informacoes_bits_variavel[k].indice_bit_mutado);
+    #endif
+
+                        // Flipa o bit
+                        populacao_de_bits[ lista_informacoes_bits_variavel[k].indice_bit_mutado ] = !populacao_de_bits[ lista_informacoes_bits_variavel[k].indice_bit_mutado ];
+
+                        // Atualiza que o bit foi flipado
+                        bit_flipado = true;
+                        break;
+                    }
+                }
+            }
+
+#if DEBUG_CONSOLE
+            Console.WriteLine("Depois da ordenação:");
+            ApresentaCromossomoBool(populacao_de_bits);
+            for (int m=0; m<depois_ordenar.Count; m++){
+                Console.WriteLine(m + ": Bit: " + depois_ordenar[m].indice_bit_mutado + " | deltaV_fitness: " + depois_ordenar[m].delta_fitness);
+            }
+#endif
 
 
 
-//                 }
+            // Retorna a nova população de bits
+            return populacao_de_bits;
+        }
 
-
-
-//             }
-
-
-// #if DEBUG_CONSOLE
-//             Console.WriteLine("Depois da ordenação");
-//             for (int i=0; i<deltaV_fitness.Count; i++){
-//                 Console.WriteLine(i + ": Bit: " + bits[i] + " | deltaV_fitness: " + deltaV_fitness[i]);
-//             }
-// #endif
-            
-//============================================================
-//             // Flipa um bit com probabilidade Pk
-//============================================================
-
-//             // Verifica as probabilidades até que um bit seja ummutado
-//             bool bit_flipado = false;
-//             while (!bit_flipado){
-
-//                 // Gera um número aleatório com distribuição uniforme
-//                 double ALE = random.NextDouble();
-
-//                 // k é o índice do array ordenado
-//                 int k = random.Next(1, tamanho_populacao_bits+1);
-                
-//                 // Com probabilidade Pk => k^(-tao)
-//                 double Pk = Math.Pow(k, -tao);
-
-//                 // k precisa ser de 1 a N, mas aqui nos índices começa em 0
-//                 k -= 1;
-// #if DEBUG_CONSOLE
-//                 // Se o Pk é maior ou igual ao aleatório, então flipa o bit
-//                 Console.WriteLine("Tentando flipar o indice "+k+", que é o bit "+bits[k]+" com Pk "+Pk+" >= ALE "+ALE);
-// #endif
-//                 if (Pk >= ALE){
-// #if DEBUG_CONSOLE
-//                     Console.WriteLine("Antes de flipar:");
-//                     ApresentaCromossomoBool(populacao_de_bits);
-// #endif
-//                     // Flipa o bit
-//                     populacao_de_bits[ bits[k] ] = !populacao_de_bits[ bits[k] ];
-// #if DEBUG_CONSOLE
-//                     Console.WriteLine("Flipando o indice " + k + ", que é o bit " + bits[k]);
-// #endif                  
-//                     // Atualiza que o bit foi flipado
-//                     bit_flipado = true;
-//                     break;
-                    
-//                 }
-//             }
-
-//             // Retorna a nova população de bits
-//             return populacao_de_bits;
-//         }
-      
 
 
 
@@ -314,7 +304,7 @@ namespace GEO_canonico
             avaliação do flip de cada bit.
             A função retorna o melhor f(x) da execução.
         */
-        public static double GEO(int n_variaveis_projeto, int bits_por_variavel, double function_min, double function_max, double tao, int criterio_parada_nro_avaliacoes_funcao, List<int> NFOBs){            
+        public static double GEO_algorithm(int n_variaveis_projeto, int bits_por_variavel_projeto, double function_min, double function_max, double tao, int criterio_parada_nro_avaliacoes_funcao, List<int> NFOBs){            
             
             //============================================================
             // Inicializa algumas variáveis de controle do algoritmo
@@ -331,7 +321,7 @@ namespace GEO_canonico
             //============================================================
             
             // Define o tamanho da população de bits
-            int tamanho_populacao_bits = n_variaveis_projeto * bits_por_variavel;
+            int tamanho_populacao_bits = n_variaveis_projeto * bits_por_variavel_projeto;
             
             // Inicializa a população de bits como uma lista de bits (bool)
             List<bool> populacao_de_bits = new List<bool>();
@@ -345,7 +335,7 @@ namespace GEO_canonico
             // Calcula o primeiro Valor Referência
             //============================================================
 
-            melhor_fx = funcao_objetivo(populacao_de_bits, n_variaveis_projeto, bits_por_variavel, function_min, function_max);
+            melhor_fx = funcao_objetivo(populacao_de_bits, n_variaveis_projeto, bits_por_variavel_projeto, function_min, function_max);
 
 #if DEBUG_CONSOLE    
             Console.WriteLine("População de bits gerado:");
@@ -363,7 +353,7 @@ namespace GEO_canonico
 #if DEBUG_CONSOLE
                 Console.WriteLine("População de Bits começo while:");
                 ApresentaCromossomoBool(populacao_de_bits);
-                double teste = funcao_objetivo(populacao_de_bits, n_variaveis_projeto, bits_por_variavel,function_min, function_max);
+                double teste = funcao_objetivo(populacao_de_bits, n_variaveis_projeto, bits_por_variavel_projeto,function_min, function_max);
                 Console.WriteLine("Fx = " + teste);
                 Console.WriteLine("Fx melhor = " + melhor_fx);
 #endif
@@ -385,7 +375,7 @@ namespace GEO_canonico
                     populacao_de_bits_flipado[i] = !populacao_de_bits_flipado[i];
 
                     // Calcula a fitness da populacao_de_bits com o bit flipado
-                    double fx = funcao_objetivo(populacao_de_bits_flipado, n_variaveis_projeto, bits_por_variavel,function_min, function_max);
+                    double fx = funcao_objetivo(populacao_de_bits_flipado, n_variaveis_projeto, bits_por_variavel_projeto, function_min, function_max);
 
                     // Calcula o ganho ou perda de flipar
                     double deltaV = fx - melhor_fx;
@@ -408,10 +398,14 @@ namespace GEO_canonico
                 // Ordena os bits e flipa apenas um deles, atualizando a população
                 //============================================================
                 
-                populacao_de_bits = GEO_ordena_e_flipa_um_bit(populacao_de_bits, lista_informacoes_mutacao, tamanho_populacao_bits, tao);
+                // populacao_de_bits = GEO_ordena_e_flipa_um_bit(populacao_de_bits, lista_informacoes_mutacao, tamanho_populacao_bits, tao);
+
+
+                populacao_de_bits = GEOvar_ordena_e_flipa_bits(populacao_de_bits, lista_informacoes_mutacao, n_variaveis_projeto, bits_por_variavel_projeto, tamanho_populacao_bits, tao);
+
 
                 // Calcula a fitness da nova população de bits
-                double fitness_populacao_de_bits = funcao_objetivo(populacao_de_bits, n_variaveis_projeto, bits_por_variavel, function_min, function_max);
+                double fitness_populacao_de_bits = funcao_objetivo(populacao_de_bits, n_variaveis_projeto, bits_por_variavel_projeto, function_min, function_max);
 
                 //============================================================
                 // Atualiza, se possível, o valor (Valor Refrência)
@@ -448,14 +442,14 @@ namespace GEO_canonico
             //============================================================
             
             // Parâmetros de execução do algoritmo
-            const int bits_por_variavel = 10;
+            const int bits_por_variavel_projeto = 10;
             const int n_variaveis_projeto = 4;
             const double function_min = -600.0;
             const double function_max = 600.0;
             
             // Se o tao é alto, é mais determinístico
             // Se o tao é baixo, é mais estocástico
-            const double tao = 0.75;
+            const double tao = 5;
             
             // Define o critério de parada com o número de avaliações da NFOBs
             const int criterio_parada_nro_avaliacoes_funcao = 200000;
@@ -464,9 +458,9 @@ namespace GEO_canonico
             // Essa lista contém todos os pontos NFOB onde se deseja saber o valor fitness do algoritmo. 
             // O algoritmo mostra o melhor fitness até o momento assim que o NFOB atinge cada um destes valores.
 
-            // List<int> NFOBs_desejados = new List<int>(){250,500,750,1000,1500,2000,3000,4000,5000,6000,7000,8000,9000,10000,15000,20000,25000,30000,40000,50000,60000,70000,80000,90000,100000,199999};
+            List<int> NFOBs_desejados = new List<int>(){250,500,750,1000,1500,2000,3000,4000,5000,6000,7000,8000,9000,10000,15000,20000,25000,30000,40000,50000,60000,70000,80000,90000,100000,199999};
             
-            List<int> NFOBs_desejados = new List<int>(){10,20,30,40,50,60,70,80,90,100,120,140,160,180,200,250,500,750,1000,1500,2000,3000,4000,5000,6000,7000,8000,9000,10000,15000,20000,25000,30000,40000,50000,60000,70000,80000,90000,100000,199999};
+            // List<int> NFOBs_desejados = new List<int>(){10,20,30,40,50,60,70,80,90,100,120,140,160,180,200,250,500,750,1000,1500,2000,3000,4000,5000,6000,7000,8000,9000,10000,15000,20000,25000,30000,40000,50000,60000,70000,80000,90000,100000,199999};
 
             //============================================================
             // Execução do algoritmo
@@ -476,7 +470,7 @@ namespace GEO_canonico
             var total_watch = System.Diagnostics.Stopwatch.StartNew();
 
             // Executa o GEO e recebe como retorno a melhor fitness da execução
-            double melhor_fx_geral = GEO(n_variaveis_projeto, bits_por_variavel, function_min, function_max, tao, criterio_parada_nro_avaliacoes_funcao, NFOBs_desejados);
+            double melhor_fx_geral = GEO_algorithm(n_variaveis_projeto, bits_por_variavel_projeto, function_min, function_max, tao, criterio_parada_nro_avaliacoes_funcao, NFOBs_desejados);
 
             // Para o temporizador
             total_watch.Stop();
